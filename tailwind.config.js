afda39b10f1316c77e5e7ee0776e855447a1494c
Dimitrije Ivanovic <dimitrije.ivanovic@student.ap.be>
2026-02-16 17:43:35 +0100

    Navbar afgewerkt

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./pages/**/*.{html,js}"],
+  content: ["../pages/**/*..{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "navcolor": "#F2F2F2",
+      },
+    },
   },
   plugins: [],
 };
