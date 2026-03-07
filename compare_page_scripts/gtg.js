0433e59a3be34b2774c833543535f30488d8cbfb
Dimitrije Ivanovic <dimitrije.ivanovic@student.ap.be>
2026-03-07 12:26:43 +0100

    Begonnen aan functies van guess the game

--- compare_page_scripts/gtg.js
+++ compare_page_scripts/gtg.js
@@ -2,29 +2,56 @@
 
 const API_KEY = "f261bf4dc1a84efeab97fb873bdedb9d";
 
-async function fetchScreenshots() {
-  const randomGamePK = Math.floor(Math.random() * 500000) + 1;
+// Game functionaliteit
+let score = 0;
+let currentGameName = "";
 
+async function fetchScreenshots() {
+  const randomGamePK = Math.floor(Math.random() * 2) + 1;
+
+  // fetch voor game
   const response = await fetch(
-    `https://api.rawg.io/api/games/${randomGamePK}/screenshots?key=${API_KEY}`,
+    `https://api.rawg.io/api/games?key=${API_KEY}&ordering=-rating&page=${randomGamePK}&page_size=20`,
   );
 
-  const data = await response.json();
+  const gamesData = await response.json();
 
-  if (!data.results || data.results.length === 0) {
+  if (!gamesData.results || gamesData.results.length === 0) {
     fetchScreenshots();
     return;
   }
 
-  const randomScreenshot =
-    data.results[Math.floor(Math.random() * data.results.length)];
+  const randomGame =
+    gamesData.results[Math.floor(Math.random() * gamesData.results.length)];
+
+  currentGameName = randomGame.name.toLowerCase();
+
+  // fetch voor screenshots
+  const ssResponse = await fetch(
+    `https://api.rawg.io/api/games/${randomGame.id}/screenshots?key=${API_KEY}`,
+  );
+  const ssData = await ssResponse.json();
+
+  if (!ssData.results || ssData.results.length === 0) {
+    fetchScreenshots();
+    return;
+  }
+
+  const randomSs =
+    ssData.results[Math.floor(Math.random() * ssData.results.length)];
 
   const img = document.createElement("img");
-  img.src = randomScreenshot.image;
-  img.style.cssText = "width:100%; height:100%; object-fit:cover;";
+  img.src = randomSs.image;
+  img.style.cssText =
+    "width:100%; height:100%; object-fit:cover; border-radius: 10px;";
 
   const gameSection = document.getElementById("game");
+  gameSection.innerHTML = "";
   gameSection.insertAdjacentElement("beforeend", img);
+}
+
+function checkGuess() {
+  const input = document.getElementById("guess-input");
 }
 
 document.addEventListener("DOMContentLoaded", () => {
