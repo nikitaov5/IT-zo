33a3240c8684247cc688533817768d84d38c9d79
Matthias Mets <s150007@ap.be>
2026-03-10 12:51:54 +0100

    Laatste build voor vandaag

--- scripts/main-page/index.js
+++ scripts/main-page/index.js
@@ -8,7 +8,6 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-console.log("TEST");
 (function () {
     return __awaiter(this, void 0, void 0, function* () {
         try {
@@ -27,7 +26,7 @@ console.log("TEST");
                     "cursor-pointer border-2 border-slate-700 rounded-xl overflow-hidden shadow-2xl transition hover:scale-[1.02] hover:border-indigo-500";
                 const img = document.createElement("img");
                 img.src = game.background_image;
-                img.className = "w-full aspect-video object-cover border border-gray-500 ";
+                img.className = "w-full object-cover border border-gray-500 ";
                 const title = document.createElement("div");
                 title.className = "p-2 text-center font-bold";
                 title.textContent = game.name;
@@ -38,7 +37,7 @@ console.log("TEST");
                     const titleClick = document.getElementById("gameName");
                     const imgClick = document.getElementById("gameImage");
                     const releaseClick = document.getElementById("gameRelease");
-                    //const playtimeClick = document.getElementById("gamePlaytime");
+                    const playtimeClick = document.getElementById("gamePlaytime");
                     const ratingClick = document.getElementById("gameRating");
                     if (titleClick)
                         titleClick.textContent = game.name;
@@ -46,8 +45,8 @@ console.log("TEST");
                         imgClick.src = game.background_image;
                     if (releaseClick)
                         releaseClick.textContent = `Released: ${game.released}`;
-                    //if (playtimeClick)
-                    //playtimeClick.textContent = `Playtime: ${game.playtime} uur`;
+                    if (playtimeClick)
+                        playtimeClick.textContent = `Playtime: ${game.playtime} uur`;
                     if (ratingClick)
                         ratingClick.textContent = `Rating: ${game.rating}`;
                 });
--- scripts/main-page/index.ts
+++ scripts/main-page/index.ts
@@ -19,11 +19,9 @@ interface Games {
   rating: number;
   playtime: number;
   rating_top: number;
-  ratings: Ratings[];
-  esrb_rating: EsrbRating[];
+  ratings: Ratings;
+  esrb_rating: EsrbRating;
 }
-
-console.log("TEST");
 
 interface ApiResponse {
   results: Games[];
@@ -50,7 +48,7 @@ interface ApiResponse {
 
       const img = document.createElement("img");
       img.src = game.background_image;
-      img.className = "w-full aspect-video object-cover border border-gray-500 ";
+      img.className = "w-full object-cover border border-gray-500 ";
 
       const title = document.createElement("div");
       title.className = "p-2 text-center font-bold";
