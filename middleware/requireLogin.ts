1e6612bef76d0d378af05835caa2e7e28661c9af
Dimitrije Ivanovic <dimitrije.ivanovic@student.ap.be>
2026-05-27 11:26:23 +0200

    Verschillende kleine tweaks, app ready to go

--- middleware/requireLogin.ts
+++ middleware/requireLogin.ts
@@ -1,8 +1,10 @@
 import { Request, Response, NextFunction } from "express";
 
-export const requireLogin = (req: Request, res: Response, next: NextFunction,) => {
-  console.log("Session in middleware:", req.session);
-  console.log("Email:", req.session.email);
+export const requireLogin = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
   if (!req.session.email) {
     return res.redirect("/login");
   }
